dcad391205198b692e6e4bb0839f31161da88025
Mayco Barale <maycobarale@gmail.com>
2024-09-19 09:07:01 -0300

    update Clock component

--- components/clock.js
+++ components/clock.js
@@ -2,19 +2,17 @@ function updateTime() {
   const timeElement = document.getElementById('time');
   const now = new Date();
 
-  timeElement.textContent = now.toLocaleTimeString('es-ES', {
+  timeElement.textContent = now.toLocaleTimeString('en-US', {
+    timeZone: 'America/Chicago',
     hour: '2-digit',
     minute: '2-digit',
-    // TODO: remove this after testing
-    second: '2-digit',
     hour12: true,
   });
 }
 
 export function startClock() {
   updateTime();
-  // setInterval(updateTime, 60000); // Luego actualiza cada minuto
-  setInterval(updateTime, 1000); // Luego actualiza cada segundo
+  setInterval(updateTime, 60000); // update every minute
 }
 
 document.addEventListener('DOMContentLoaded', startClock);
